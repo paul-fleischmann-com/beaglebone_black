8c95f160a7749ee04c957dc124667e0478864ac5
paulefl <paul.fleischmann81@gmail.com>
2026-03-24 08:22:47 +0000

    bump code für linux-embedded-hal=0.4 und bme280 = 0.5 kombatbilität

--- project/rust-lib/src/bme280.rs
+++ project/rust-lib/src/bme280.rs
@@ -1,8 +1,7 @@
 use std::ffi::CStr;
 use libc::c_char;
 use bme280::i2c::BME280;
-use linux_embedded_hal::I2cdev;
-use embedded_hal::i2c::I2c as BlockingI2c;
+use linux_embedded_hal::{Delay, I2cdev};
 
 const SEA_LEVEL_PA: f64 = 101325.0;
 
@@ -25,76 +24,28 @@ fn err(code: i32) -> RsBme280Data {
                    altitude: 0.0, timestamp: 0, error: code }
 }
 
-// Wrap blocking I2cdev to implement embedded-hal-async's I2c trait
-struct SyncI2c(I2cdev);
-
-impl embedded_hal::i2c::ErrorType for SyncI2c {
-    type Error = <I2cdev as embedded_hal::i2c::ErrorType>::Error;
-}
-
-impl embedded_hal::i2c::I2c for SyncI2c {
-    fn read(&mut self, addr: u8, buf: &mut [u8]) -> Result<(), Self::Error> {
-        BlockingI2c::read(&mut self.0, addr, buf)
-    }
-    fn write(&mut self, addr: u8, buf: &[u8]) -> Result<(), Self::Error> {
-        BlockingI2c::write(&mut self.0, addr, buf)
-    }
-    fn write_read(&mut self, addr: u8, w: &[u8], r: &mut [u8]) -> Result<(), Self::Error> {
-        BlockingI2c::write_read(&mut self.0, addr, w, r)
-    }
-    fn transaction(&mut self, addr: u8, ops: &mut [embedded_hal::i2c::Operation<'_>]) -> Result<(), Self::Error> {
-        BlockingI2c::transaction(&mut self.0, addr, ops)
-    }
-}
-
-impl embedded_hal_async::i2c::I2c for SyncI2c {
-    async fn read(&mut self, addr: u8, buf: &mut [u8]) -> Result<(), Self::Error> {
-        BlockingI2c::read(&mut self.0, addr, buf)
-    }
-    async fn write(&mut self, addr: u8, buf: &[u8]) -> Result<(), Self::Error> {
-        BlockingI2c::write(&mut self.0, addr, buf)
-    }
-    async fn write_read(&mut self, addr: u8, w: &[u8], r: &mut [u8]) -> Result<(), Self::Error> {
-        BlockingI2c::write_read(&mut self.0, addr, w, r)
-    }
-    async fn transaction(&mut self, addr: u8, ops: &mut [embedded_hal::i2c::Operation<'_>]) -> Result<(), Self::Error> {
-        BlockingI2c::transaction(&mut self.0, addr, ops)
-    }
-}
-
-// Wrap blocking sleep to implement embedded-hal-async's DelayNs trait
-struct SyncDelay;
-
-impl embedded_hal_async::delay::DelayNs for SyncDelay {
-    async fn delay_ns(&mut self, ns: u32) {
-        std::thread::sleep(std::time::Duration::from_nanos(ns as u64));
-    }
-}
-
 /// # Safety
 #[no_mangle]
 pub unsafe extern "C" fn rs_bme280_read(i2c_path: *const c_char, addr: u8) -> RsBme280Data {
     let path = match CStr::from_ptr(i2c_path).to_str() { Ok(p) => p.to_owned(), Err(_) => return err(-1) };
     let i2c  = match I2cdev::new(&path)                 { Ok(d) => d,            Err(_) => return err(-2) };
-    let mut s = BME280::new(SyncI2c(i2c), addr);
-    let mut d = SyncDelay;
-    futures::executor::block_on(async move {
-        if s.init(&mut d).await.is_err() { return err(-4); }
-        match s.measure(&mut d).await {
-            Ok(m) => {
-                let ph = m.pressure as f64 / 100.0;
-                RsBme280Data {
-                    temperature: (m.temperature as f64 * 100.0).round() / 100.0,
-                    humidity:    (m.humidity    as f64 * 100.0).round() / 100.0,
-                    pressure:    (ph * 100.0).round() / 100.0,
-                    altitude:    (calc_altitude(ph) * 10.0).round() / 10.0,
-                    timestamp:   std::time::SystemTime::now()
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .unwrap_or_default().as_secs(),
-                    error: 0,
-                }
+    let mut s = BME280::new(i2c, addr);
+    let mut d = Delay;
+    if s.init(&mut d).is_err() { return err(-4); }
+    match s.measure(&mut d) {
+        Ok(m) => {
+            let ph = m.pressure as f64 / 100.0;
+            RsBme280Data {
+                temperature: (m.temperature as f64 * 100.0).round() / 100.0,
+                humidity:    (m.humidity    as f64 * 100.0).round() / 100.0,
+                pressure:    (ph * 100.0).round() / 100.0,
+                altitude:    (calc_altitude(ph) * 10.0).round() / 10.0,
+                timestamp:   std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap_or_default().as_secs(),
+                error: 0,
             }
-            Err(_) => err(-5),
         }
-    })
+        Err(_) => err(-5),
+    }
 }
