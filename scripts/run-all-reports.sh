64594e9dfbdead42209eed147cead9d2b89c805c
paulefl <paul.fleischmann81@gmail.com>
2026-04-13 16:53:37 +0000

    improve drone pipe handling

--- scripts/run-all-reports.sh
+++ scripts/run-all-reports.sh
@@ -16,7 +16,9 @@ report 3-tools           lint-cli lint-tui build-cli-linux-amd64 build-cli-linux
 report 4-webapp          validate-html package-web-gui
 report 5-release         build-arm build-tools build-web-gui checksums build-docker-image
 report 6-nightly         test-all-backends
-report 7-reports         go-tests-with-coverage python-tests python-security shellcheck c-coverage adoc-build strictdoc-export prepare-reports print-url strictdoc-coverage-gate traceability-check sonarqube-report sonarcloud-scan collect-and-report quality-gates package-artifacts
+report 7a-testing        go-tests-with-coverage python-tests c-coverage sonarqube-report sonarcloud-scan strictdoc-coverage-gate traceability-check
+report 7b-analysis       python-security shellcheck
+report 7c-docs           adoc-build strictdoc-export prepare-reports upload print-url
 report 8-bausteinsicht   validate-model export-plantuml export-mermaid export-table export-png commit-exports
 report 9-build-container build-generic-builder clone-bausteinsicht-repo build-bausteinsicht build-strictdoc
 report 10-integration-tests download-release-assets test-hardware test-api test-uart-on-bbb test-cli-remote promote-to-stable
