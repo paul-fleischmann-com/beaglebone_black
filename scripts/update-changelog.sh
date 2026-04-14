dc3e16f35169f637428e491a3f5da49b6bd83a90
paulefl <paul.fleischmann81@gmail.com>
2026-04-14 16:23:01 +0000

    fix(ci): GitHub repo URL hardcodiert, DRONE_GIT_HTTP_URL entfernt

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -23,5 +23,5 @@ if git diff --cached --quiet; then
   echo "Keine Changelog-Änderungen"
 else
   git commit -m "docs(changelog): update unreleased section [skip ci]"
-  git push https://paulefl:${GIT_TOKEN}@${DRONE_GIT_HTTP_URL#https://} HEAD:main
+  git push https://paulefl:${GIT_TOKEN}@github.com/paulefl/beaglebone_black.git HEAD:main
 fi
